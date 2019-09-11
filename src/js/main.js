cb60e671ec8d5a3e2f1fe59e36ae7db5487aa196
przonsow <piotr.rzonsowski@cognitran.com>
2019-09-11 11:18:02 +0200

    Add 4 last updated repository to list of projects

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,44 @@
 
 console.log(`Run 🏃‍♂️🏃‍♂️`);
 
+const listOfProjects = document.querySelector('.cards-js');
+
 fetch('https://api.github.com/users/rzonsol/repos?sort=updated&per_page=4')
 	.then(resp => resp.json())
 	.then(resp => {
-		console.log('Data download!!!');
-		console.table(resp);
+		resp.forEach(element => {
+			const { description, homepage, html_url, name } = element;
+
+			const linkToDemoPage =
+				homepage != null
+					? `<a class="cards__item__footer__a" href="${homepage}">
+        Demo
+      </a>
+      `
+					: '';
+
+			listOfProjects.innerHTML += `<section class="cards__item">
+							<div class="cards__item__container">
+								<img
+									src="../assets/img/github_Icon.svg"
+									alt="Icons of GitHub"
+								/>
+								<h4 class="cards__item__title">${name}</h4>
+								<p class="cards__item__description">${
+									description != null ? description : 'No description.'
+								}</p>
+							</div>
+							<footer class="cards__item__footer">
+								${linkToDemoPage}
+								<a
+									href="${html_url}"
+									class="cards__item__footer__a cards__item__footer__a__code"
+								>
+									GitHub
+								</a>
+							</footer>
+						</section>`;
+		});
 	})
 	.catch(errors => {
 		console.log(errors);
